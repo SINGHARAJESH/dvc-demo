36b5f0f1312c66b429ea8a31538a44b52c43ce49
SINGHARAJESH <rajeshsingha1947@gmail.com>
2025-05-13 09:57:09 +0530

    second exp

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,7 +4,9 @@ import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 df = df.iloc[:, 3:]
 
-df = df[df['Length of Membership'] > 3]
+df = df[df['Length of Membership'] > 1]
+# drop first columns
+df.drop(columns=['Avg. Session Length'],inplace=True)
 # Step 4: Save to CSV
 file_path = os.path.join('data', 'customer.csv')
 df.to_csv(file_path, index=False)
